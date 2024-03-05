cd05751d2dbe002dea6b81e867e5f4ca851a52b1
dahye <ghkt2535@naver.com>
2024-03-05 17:22:56 +0900

    String

---
+++ studyjava/src/JavaStr.java
@@ -0,0 +1,16 @@
+public class JavaStr {
+    public static void main(String[] args) {
+        // 문자열의 '+' 연산
+        String a1 = "hello";
+        String a2 = "World";
+        System.out.println(a1 + " " + a2);
+
+        // 문자열과 숫자의 '+' 연산
+        System.out.println(1 + "hi");
+        System.out.println(1 + 2 + "hi");
+        System.out.println("hi" + 1 + 2);
+        System.out.println("hi" + (1 +2));
+    }
+}
+
+
